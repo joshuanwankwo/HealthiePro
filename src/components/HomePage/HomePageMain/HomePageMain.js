acd4c2d775072addb58608b224dda37359eff2bd
Agent Peter <peteryar19@gmail.com>
2019-11-12 11:44:32 +0100

    responsive fixes

--- src/components/HomePage/HomePageMain/HomePageMain.js
+++ src/components/HomePage/HomePageMain/HomePageMain.js
@@ -6,18 +6,20 @@ import HomePageNav from './HomePageNav/HomePageNav';
 class HomePageMain extends React.Component {
     constructor(props) {
         super(props);
-        this.state={}
+        this.state = {}
     }
 
     render() {
         return (
             <div id="homeMain-con">
                 <HomePageNav />
-                <section id="home-intro">
-                    <p>Every day we bring hope </p>
-                    <p>and smile to Patient we serve</p>
-                </section>
-                 <HomeCards />
+                <div id="">
+                    <section id="home-intro">
+                        <p>Every day we bring hope </p>
+                        <p>and smile to Patient we serve</p>
+                    </section>
+                    <HomeCards />
+                </div>
             </div>
         )
     }
