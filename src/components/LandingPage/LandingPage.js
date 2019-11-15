b6485caac02fc0611ed872f92aa2d9b35cc022e9
saucecodee <ashawinta@gmail.com>
2019-11-15 08:01:35 +0100

    edited landing page

--- src/components/LandingPage/LandingPage.js
+++ src/components/LandingPage/LandingPage.js
@@ -9,18 +9,21 @@ import Solutions from '../HomePage/Solutions/Solutions';
 import Team from '../HomePage/Team/Team';
 import { Switch, Route, BrowserRouter, Link } from "react-router-dom";
 
+import landingImg from '../../assets/landing-side-img.png'
+import docImg from '../../assets/doc-pic-landing.png'
+
 import cloudinary from 'cloudinary-core';
 const cloudinaryCore = new cloudinary.Cloudinary({ cloud_name: 'healthie' });
 
 const footerStyles = {
     container: {
-        margin:0,
+        margin: 0,
         backgroundColor: '#35444E',
 
     },
-    textCon:{
-        padding:0,
-        border:'none'
+    textCon: {
+        padding: 0,
+        border: 'none'
     },
     textColor: {
         color: '#fff',
@@ -30,48 +33,49 @@ const footerStyles = {
 
 function LandingPage(props) {
 
-
     return (
-    <div className="LandingPage-parent ">
-
-        <NavBarLandPage />
-
-        <div className="landPage-content">
-
-        <div className="parent-text-btn">
-
-            <h1 className="h1-tag">
-                We are committed to your health
-        </h1>
-
-            <p className="p-tag">
-                Since the first day of operation Healthie, our team has been
-                focused on building a high-quality healthcare services
-        </p>
-
-
-            <div>
-                <Button bg={'#4290C9'} buttonName={`Book Appointment`} width={`290px`} height={`59px`} marginTop={`500px`} marginLeft={`60px`} padding={`20px`} />
+        <div>
+            <NavBarLandPage />
+            <div id="home" className="container landing-jumbotron">
+                <div className="ladning-jumbo-left">
+                    <h1>We are committed to your health</h1>
+                    <p>Since the first day of operation Healthie, our team has been focused on building a high-quality healthcare services</p>
+                    <button className="pri-but">Book Appointment</button>
+                </div>
+                <img className="doctors-img" src={landingImg} />
             </div>
+
+            <div id="about-us" className="container landing-details">
+                <img src={docImg} />
+                <div className="landing-details-content">
+                    <h3>About Us</h3>
+                    <h2>Professional Health Care in Full Measure</h2>
+                    <p>For us there are no minor aspect, because a quality result always depends on triflies. over the years of our activities, we have gained a unique experience of organizing health care services for our citizens. Private patients and business corporations feel safe and comfortable in choosing us as their no. 1 healthcare service provider.</p>
+                </div>
+            </div>
+            <div id="service" className="container landing-details">
+                <div className="landing-details-content">
+                    <h3>Our Services</h3>
+                    <h2>We Provide you with the best health care services</h2>
+                    <p>We are compassionate about the community and the way their health affect their everyday activity with their environment and with one another.<br /><br /> Therefore our services ranging from the simplest first aid treatments to complex consultions with our medical professionals are available nationwide to all as our priority is to create a healthy and happier world.</p>
+                </div>
+                <img src={docImg} />
+            </div>
+            <div id="our-solutions" className="container landing-details">
+                <img src={docImg} />
+                <div className="landing-details-content">
+                    <h3>Our Solutions</h3>
+                    <h2>We provide the best quality of healthcare for the communities.</h2>
+                    <p>For us there are no minor aspect, because a quality result always depends on triflies. over the years of our activities, we have gained a unique experience of organizing health care services for our citizens.<br /><br /> Private patients and business corporations feel safe and comfortable in choosing us as their no. 1 healthcare service provider.</p>
+                </div>
+            </div>
+
+
+            <Team />
+            <Footer styles={footerStyles} />
+
         </div>
-    
-
-        <div className="langpage-image">
-             <img className="doctors-img" src={cloudinaryCore.url('https://res.cloudinary.com/healthie/image/upload/v1573230072/healthie/Img_kwifbg.png')} style={{maxHeight: '650px', maxWidth:'450px'}}/>
-        </div>
-
-        </div>
-        
-
-        <About />
-        <Services />
-        <Solutions />
-        <Team />
-        <Footer styles={footerStyles} />
-
-    </div>
-
-        )
+    )
 }
 
 
